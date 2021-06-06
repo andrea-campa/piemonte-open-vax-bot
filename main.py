be80d9b2f97187fbe6958ecfd3a6aaae0a7e99ef
itscampa <mac.aerdna@gmail.com>
2021-06-06 23:25:04 +0200

    intermediate

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from requests.api import get
 import confidential
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import json
-import smtplib
 import time 
 
 def sendMessage (chat_id, text, parse_mode, no_link_preview):
@@ -27,36 +28,75 @@ def getUpdates ():
     print(response.status_code)
     return response
 
-def check_message (mes):
+def check_message (mes, id):
+
     print(mes)
-    if (mes.find('/help')!=-1):
-        sendMessage(confidential.id_privatechat, '*the fuck d\'ya want*', 'Markdown', True)
-    elif (mes.find('/ping')!=-1):
-        sendMessage(confidential.id_privatechat, '*ping pong, king kong*', 'Markdown', True)
+    if (mes.find('/start')!=-1):
+        sendMessage(id, 'use /register to register to updates', 'Markdown', True)
 
-def check_pinebook ():
-    print ('Checking pinebook')
-    text='14″ PINEBOOK Pro LINUX LAPTOP (ANSI, US Keyboard) [Out of Stock]' 
-    product_link = 'https://pine64.com/product/14%E2%80%B3-pinebook-pro-linux-laptop-ansi-us-keyboard/'
-    page=requests.get(product_link)
-    soup = BeautifulSoup (page.content, 'html.parser')
-    #print(soup.prettify())
-    html = soup.find('h1').getText()
+    elif (mes.find('/register')!=-1):
+        with open('mailing_list.txt', 'r+', encoding = 'utf-8') as file:
+            file.write(str(id) + '\n')
+        sendMessage(id, '**bot goes brrrr** you can go relax!', 'Markdown', True)
 
-    if (html!=text):
+def check_website_change ():
 
-        #do things
-        print('Something changed!')
-        text = 'Something changed on the PineBook page you were watching \n[link here](' + product_link + ')'
-        response = sendMessage (confidential.id_privatechat, text, 'Markdown', True)
-        return response
+    trigger = 0
+    f = open("reference_page.html", "r" , encoding = 'utf-8')
+    g = open("screenshot.png", "wb")
+    
+    chrome_options = Options()
+    chrome_options.add_argument("--user-data-dir=chrome-data")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=720,1080")
+    chrome_options.add_argument("--hide-scrollbars")
 
-    else:
-        print('All the same :(')
-        return 0
+    driver = webdriver.Chrome('./chromedriver',options=chrome_options)
+
+    print ('Checking webiste')
+    site_link = 'https://www.ilpiemontetivaccina.it/preadesione/#/'
+    driver.get(site_link)
+
+    element = driver.find_element_by_id("vday")
+    html = element.get_attribute('innerHTML')
+
+    time.sleep(1)
+    if (f.read() != html):
+        trigger = 1
+        element.screenshot_as_png #to take the page to the rigth place
+
+        #taking screenshot of "vday" section
+        print('Taking screenshot')
+        driver.execute_script("document.getElementById('outdated').innerHTML = '';")
+        time.sleep(4)
+        element_png = element.screenshot_as_png
+        g.write(element_png)
+        driver.quit()
+
+    f.close()
+    g.close()
+
+    return trigger
+
+    #html = soup.find('body').getText()
+
+    # if (html!=text):
+
+    #     #do things
+    #     print('Something changed!')
+    #     text = 'Something changed on the PineBook page you were watching \n[link here](' + product_link + ')'
+    #     response = sendMessage (confidential.id_privatechat, text, 'Markdown', True)
+    #     f.close()
+    #     return response
+
+    # else:
+    #     print('All the same :(')
+    #     f.close()
+    #     return 0
 
 
 f = open("last_message_id.txt", "r+" , encoding = 'utf-8')
+
 counter = 0
 
 while True:
@@ -65,11 +105,12 @@ while True:
     counter = counter % 100
     response = getUpdates()
     a = json.loads(response.text)
-    #print(json.dumps(a['result'], indent=4, sort_keys=True))
+    print(json.dumps(a['result'], indent=4, sort_keys=True))
 
     text = a['result'][0]['message']['text']
     username = a['result'][0]['message']['chat']['username']
     message_id = a['result'][0]['message']['message_id']
+    user_id = a['result'][0]['message']['chat']['id']
     
     # ______                                                  
     # | ___ \                                                 
@@ -81,7 +122,7 @@ while True:
     #                               |___/               |___/ 
 
     if (counter==1):
-        check_pinebook ()
+        check_website_change()
 
     #   ___                                  
     #  / _ \                                 
@@ -89,10 +130,11 @@ while True:
     # |  _  | '_ \/ __\ \ /\ / / _ \ '__/ __|
     # | | | | | | \__ \\ V  V /  __/ |  \__ \
     # \_| |_/_| |_|___/ \_/\_/ \___|_|  |___/
-                                                                                 
+                                             
     f.seek (0)
     history=f.read()
-        
+    
+    #checking if last message has already been seen
     if (str(message_id) != history):
         print('New Message')
         check = 1 
@@ -100,11 +142,12 @@ while True:
         print('Same Message')
         check = 0
 
-    if (username==confidential.tg_username and check):
-        print('It\'s you!')
+    #answer if it's a new message
+    if (check):
+        print('Answering!')
         f.seek (0)
         f.write(str(message_id))
-        check_message(text)
-        #sendMessage(confidential.id_privatechat, text, 'Markdown', True)
+        check_message(text, user_id)
+        sendMessage(confidential.id_privatechat, text, 'Markdown', True)
 
 
